662e3dfdf6b744141d1e7132cc9be0c9b9c1eab3
Robert Davis <rdavis4677@gmail.com>
2018-05-14 15:17:09 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# twitchbot
+This is a project for connecting twitch to an raspberry pi 
+
+The code is based off of https://pimylifeup.com/raspberry-pi-twitch-bot/
+
+ 
---
+++ config.py
@@ -0,0 +1,11 @@
+HOST = "irc.twitch.tv"              # This is Twitchs IRC server
+PORT = 6667                         # Twitchs IRC server listens on port 6767
+NICK = "bdavsbot"            # Twitch username your using for your bot
+PASS = "oauth:hk62g8etd3t44q5trcee9ggztiq4i2" # your Twitch OAuth token
+CHAN = "#bdavs77"                   # the channel you want the bot to join.
+RATE = (20/30) # messages per seccond
+BAN_PAT = [
+    r"test"
+#    r"swear",
+#    r"some_pattern"
+]
