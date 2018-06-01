fdde2ab3022cce43b7d89e9d608ddcc1110032fe
Robert Davis <rdavis4677@gmail.com>
2018-05-31 22:29:23 -0400

    still adding missed files

--- config.py
+++ config.py
@@ -3,19 +3,21 @@ HOST = "irc.twitch.tv"              # This is Twitchs IRC server
 PORT = 6667                         # Twitchs IRC server listens on port 6767
 NICK = "bdavsbot"            # Twitch username your using for your bot
 PASS = "oauth:hk62g8etd3t44q5trcee9ggztiq4i2" # your Twitch OAuth token
-#CHAN = "#bdavs77"
+CHAN = "#bdavs77"
 #CHAN = "#accidentalgrenade"                   # the channel you want the bot to join.
 #CHAN = "#huttsgaming"
 #CHAN = "#magnusrayneg4l"
-CHAN = "#chinoxg4l"
+#CHAN = "#chinoxg4l"
 RATE = (20/30) # messages per seccond
 SEARCH_PAT = [
     r"test"
 #    r"swear",
 #    r"some_pattern"
 ]
+MOD_COMMAND = r"!setmessage"
 
 #interface config
 SPEEDX = 5
 SPEEDY = 5
 RPI = 0
+TIMEOUT = 1000
